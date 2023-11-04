eca744bf6f6da35a33c8ff6f2ddd034cb81145b5
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-11-04 18:11:47 +0100

    implement gravity and jump

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -4,12 +4,38 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Transform groundCheck;
+    //public Animator animator;
+
     public float speed = 5f;
     public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 1f;
+
     float turnSmoothVelocity;
+    Vector3 velocity;
+    bool isGrounded;
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * gravity * -2f);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontalAxis, 0f, verticalAxis).normalized;
@@ -23,5 +49,6 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(speed * Time.deltaTime * moveDirection.normalized);
         }
+
     }
 }
