9643a2bafc1c7139d1c46da46f450a1dd6dc7496
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-11-09 22:56:25 +0100

    add enemy animations

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,6 +4,9 @@ public class Target : MonoBehaviour
 {
     public int health = 100;
     public HealthBar healthBar;
+    public Animator targetAnimator;
+
+    private bool isDying;
 
     void Start()
     {
@@ -12,8 +15,16 @@ public class Target : MonoBehaviour
 
     public void TakeDamage(int amount)
     {
+        if (isDying)
+        {
+            return;
+        }
+
+        targetAnimator.Play("GetHit");
+
         health -= amount;
         healthBar.SetHealth(health);
+
         if (health <= 0f)
         {
             Die();
@@ -22,6 +33,12 @@ public class Target : MonoBehaviour
 
     void Die()
     {
-        Destroy(gameObject);
+        isDying = true;
+
+        targetAnimator.Play("Die");
+
+        float deathAnimationLength = targetAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+        Destroy(healthBar.gameObject);
+        Destroy(gameObject, deathAnimationLength);
     }
 }
