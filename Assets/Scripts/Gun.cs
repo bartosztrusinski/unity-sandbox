8c51eeeb768442977a3068d466974c2fc5a008a7
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-11-09 16:53:36 +0100

    create SMG

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,16 +4,20 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
     public float impactForce = 30f;
 
     public Camera fpsCamera;
     public ParticleSystem muzzleFlash;
     public ParticleSystem impactEffect;
 
+    private float nextTimeToFire = 0f;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
