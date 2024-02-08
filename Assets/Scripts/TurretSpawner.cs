85f5a4655babe7790c6e7dc6ccd99f72c8bfeadf
Bartosz Trusinski <bartosztrusinski@gmail.com>
2024-02-08 23:43:57 +0100

    bring back ability to spawn turrets; fix player size; fix weapon size & position

--- Assets/Scripts/TurretSpawner.cs
+++ Assets/Scripts/TurretSpawner.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TurretSpawner : MonoBehaviour
 {
     public PlayerEnergy playerEnergy;
     public GameObject turretPrefab;
     public int turretCost = 30;
+    private bool isTurretSpawn = false;
+
+
+    public void OnTurretSpawn(InputAction.CallbackContext context)
+    {
+        isTurretSpawn = true;
+    }
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (isTurretSpawn)
         {
+            isTurretSpawn = false;
             if (playerEnergy.currentEnergy >= turretCost)
             {
                 Vector3 spawnPosition = transform.position + transform.right * 2f;
